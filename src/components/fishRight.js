f1fbc0b114e82ec7a5c1a257d89effd7974f518c
Ollie-Judge <ollie.judge@hotmail.com>
2023-03-27 18:24:37 +0100

    added fish video as a br

--- src/components/fishRight.js
+++ src/components/fishRight.js
@@ -1,11 +1,14 @@
 import { Grid } from "@mui/material";
+import fish from "../resources/f504eafe9917f0b39a40932f7e548813.mp4";
 
 function FishRight() {
   return (
     <div className="App">
       <Grid container>
         <Grid item xs={12}>
-          <img alt="fish background" src=""></img>
+          <video loop autoPlay muted style={{ width: "100%" }}>
+            <source src={fish} type="video/mp4" />
+          </video>
         </Grid>
       </Grid>
     </div>
