5027b59380ac4a08f587d9539eea824a1477877b
Ollie-Judge <ollie.judge@hotmail.com>
2023-03-27 18:03:12 +0100

    added download on appstore and google play buttons

--- src/components/download.js
+++ src/components/download.js
@@ -1,4 +1,4 @@
-import { Button, Grid, Typography } from "@mui/material";
+import { Button, Grid, Link, Typography } from "@mui/material";
 
 function Download() {
   return (
@@ -14,10 +14,40 @@ function Download() {
             </Typography>
           </Grid>
           <Grid item xs={12}>
-            <Button></Button>
+            <Link
+              href="https://apps.apple.com/us/app/google/id284815942?itsct=apps_box_badge&amp;itscg=30200"
+              style={{
+                display: "inline-block",
+                overflow: "hidden",
+                borderRadius: "13px",
+                width: "250px",
+                height: "83px",
+              }}
+            >
+              <img
+                src="https://tools.applemediaservices.com/api/badges/download-on-the-app-store/black/en-us?size=250x83&amp;releaseDate=1549929600"
+                alt="Download on the App Store"
+                style={{ borderRadius: "13px", width: "250px", height: "83px" }}
+              ></img>
+            </Link>
           </Grid>
           <Grid item xs={12}>
-            <Button></Button>
+            <Link
+              href="https://play.google.com/store/apps?pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1"
+              style={{
+                display: "inline-block",
+                overflow: "hidden",
+                borderRadius: "13px",
+                width: "250px",
+                height: "83px",
+              }}
+            >
+              <img
+                alt="Get it on Google Play"
+                src="https://play.google.com/intl/en_us/badges/static/images/badges/en_badge_web_generic.png"
+                style={{ borderRadius: "13px", width: "250px", height: "83px" }}
+              ></img>
+            </Link>
           </Grid>
         </Grid>
         <Grid item xs={12} md={6}>
